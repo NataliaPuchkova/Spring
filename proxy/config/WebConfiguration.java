8f726b9e2e8b6545efface9093a059834acd4244
Natalia Puchkova <35714707+NataliaPuchkova@users.noreply.github.com>
2019-04-11 19:52:53 -0500

    Create WebConfiguration.java

---
+++ proxy/config/WebConfiguration.java
@@ -0,0 +1,18 @@
+package proxy.config;
+
+import org.h2.server.web.WebServlet;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+// added for H2 console
+@Configuration
+public class WebConfiguration {
+	@Bean
+	ServletRegistrationBean h2servletRegistration() {
+		ServletRegistrationBean registrationBean = new ServletRegistrationBean(new WebServlet());
+		registrationBean.addUrlMappings("/console/*");
+		return registrationBean;
+	}
+
+}
