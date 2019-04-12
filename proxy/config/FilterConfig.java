927af63f49cd96ed9e4d1672d9e35275d60dbcb0
Natalia Puchkova <35714707+NataliaPuchkova@users.noreply.github.com>
2019-04-11 19:52:20 -0500

    Create FilterConfig.java

---
+++ proxy/config/FilterConfig.java
@@ -0,0 +1,47 @@
+package proxy.config;
+
+import proxy.filters.APIFilter;
+import proxy.filters.PostFilter;
+import proxy.filters.PreFilter;
+import proxy.route.model.FunctionalZone;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@ConfigurationProperties
+@Configuration
+public class FilterConfig {
+	@Value("${environments.urlz1}")
+	private String URL_UAT_Z2;
+	@Value("${environments.urlz2.1}")
+	private String URL_UAT2_ZONE_1;
+	@Value("${environments.urlz2.2}")
+	private String URL_UAT2_ZONE_2;
+
+	@Bean
+	public PreFilter preFilter() {
+		return new PreFilter();
+	}
+
+	@Bean
+	public PostFilter postFilter() {
+		return new PostFilter();
+	}
+
+	@Bean
+	public APIFilter apiFilter() {
+		return new APIFilter();
+	}
+
+	@Bean(name = "zone1")
+	public FunctionalZone beanZone1() {
+		return new FunctionalZone("", URL_UAT_Z2);
+	}
+
+	@Bean(name = "zone2")
+	public FunctionalZone beanZone2() {
+		return new FunctionalZone(URL_UAT2_ZONE_1, URL_UAT2_ZONE_2);
+	}
+
+}
